d32bcb42846e3aa32807fc19c4ca3a04547f33c1
Evgeny A. Mamonov <evgeny@mamonov.org>
2021-08-18 02:15:17 +0300

    initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="text-center">
+    <h1 class="text-primary">EVIS Data Marketplace</h1>
+    <img
+      alt="EVIS Data MarketPlace"
+      src="~assets/apple-touch-icon.png"
+      style="width: 180px; height: 180px"
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex',
+});
+</script>
