7345786aed0ddae5ae059c9546fd9244db098466
Evgeny A. Mamonov <evgeny@mamonov.org>
2021-09-02 15:54:08 +0300

    Refactoring

---
+++ src/components/AppLayout/SearchAutocomplete.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-input
+    v-model="search"
+    debounce="500"
+    outlined
+    placeholder="Search"
+  >
+    <template v-slot:append>
+      <q-icon name="search" />
+    </template>
+  </q-input>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'SearchAutocomplete',
+  data() {
+    return {
+      search: '',
+    };
+  },
+});
+</script>
--- src/components/CategoryPreview.vue
+++ src/components/CategoryPreview.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="category-preview">
-    <div class="row justify-between items-center q-mb-lg">
-      <img src="../assets/pulse-icon.svg" alt="Category icon" class="q-mr-sm q-ml-lg">
-      <h5>Currency Data</h5>
+    <div
+      class="row items-center q-mb-lg"
+      :class="logo ? 'justify-between' : 'justify-end'"
+    >
+      <img
+        v-if="logo"
+        :src="logo"
+        alt="Category icon"
+        width="50"
+        class="q-mr-sm q-ml-lg"
+      >
+      <h5>{{ category.name }}</h5>
     </div>
-    <p class="q-mb-lg">
-      Data description Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    </p>
-    <div class="row">
+    <p v-if="description" class="q-mb-lg">{{ description }}</p>
+    <div class="row" v-if="recommendedFor.length">
       <div
-        v-for="categoryType in categoryTypes"
-        :key="categoryType"
+        v-for="recommendedType in recommendedFor"
+        :key="recommendedType.id"
         class="col-6 row items-center q-mb-sm"
       >
         <q-icon name="done" class="q-mr-lg done-icon" />
-        <p class="q-mb-none">{{ categoryType }}</p>
+        <p class="q-mb-none">{{ recommendedType.name }}</p>
       </div>
     </div>
   </div>
@@ -23,14 +29,19 @@
 
 <script>
 export default {
-  data() {
-    return {
-      categoryTypes: [
-        'For traders', 'For analytics', 'For scientists', 'For personal use',
-      ],
-    };
-  },
+  props: ['category'],
   name: 'CategoryPreview',
+  computed: {
+    logo() {
+      return this.category.logo_url;
+    },
+    recommendedFor() {
+      return this.category.recommended_for;
+    },
+    description() {
+      return this.category.descr;
+    },
+  },
 };
 </script>
 
@@ -40,13 +51,12 @@ export default {
     font-style: normal;
     font-weight: 600;
     font-size: $size;
-    line-height: 20px;
+    line-height: 1em;
     color: $color;
   }
   h5 {
     @include fontStyles(36px);
-    margin: 0;
-    margin-right: 40px;
+    margin: 0 40px 0 0;
   }
   p, .done-icon {
     @include fontStyles(18px);
