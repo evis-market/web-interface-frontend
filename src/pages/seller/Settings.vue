0b7b532a203e8b901cf3a0dfa362886c3f318b75
Andrei Popov <andrepopoff@gmail.com>
2021-09-04 13:06:22 +0300

    [EF-26] Add some fields to the seller settings form

--- src/pages/seller/Settings.vue
+++ src/pages/seller/Settings.vue
@@ -3,7 +3,45 @@
     <div class="q-gutter-y-md">
       <TabMenu tab="seller"/>
       <SellerTabs tab="settings"/>
-      [SAVE SETTINGS FORM]
+      <q-form class="q-gutter-md q-pa-md">
+        <div class="row">
+          <div class="col-lg-1 col-xs-3">
+            <q-field borderless label="Display Name" readonly />
+          </div>
+          <div class="col">
+            <q-input v-model="name" />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-1 col-xs-3">
+            <q-field borderless label="Description" readonly />
+          </div>
+          <div class="col">
+            <q-input v-model="description" type="textarea" />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-1 col-xs-3">
+            <q-field borderless label="Logo" readonly />
+          </div>
+          <div class="col">
+            <q-file outlined v-model="logo">
+              <template v-slot:prepend>
+                <q-icon name="attach_file" />
+              </template>
+            </q-file>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-1 col-xs-3">
+            <q-field borderless label="Site" readonly />
+          </div>
+          <div class="col">
+            <q-input v-model="site" />
+            <q-input v-model="site" />
+          </div>
+        </div>
+      </q-form>
     </div>
   </div>
 </template>
@@ -16,6 +54,11 @@ export default {
   name: 'PageSellerSettings',
   data() {
     return {
+      name: 'TovoData',
+      description: 'We sell data.\n'
+        + 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut  labore et dolore magna aliqua.\n',
+      logo: null,
+      site: 'tovodata.com',
     };
   },
   components: {
