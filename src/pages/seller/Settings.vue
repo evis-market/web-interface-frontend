238948efee25fef719bcec49866468d4e12ccca4
Andrei Popov <andrepopoff@gmail.com>
2021-09-09 22:21:59 +0300

    [EF-28] Add settings form validators and show server errors

--- src/pages/seller/Settings.vue
+++ src/pages/seller/Settings.vue
@@ -6,25 +6,43 @@
       <div class="full-width row justify-center">
         <q-card class="col-lg-9 col-md-10 col-xs-12">
           <q-card-section>
-            <q-form class="q-gutter-md q-pa-md">
-              <div class="row items-center">
-                <q-icon name="person" class="col-auto q-mr-md" size="sm" />
-                <q-input dense v-model="name" label="Display Name" class="col" />
+            <q-form class="q-pa-md">
+              <div class="row">
+                <q-icon name="person" class="col-auto q-mr-md q-mt-sm" size="sm" />
+                <q-input
+                  dense
+                  v-model="name"
+                  label="Display Name"
+                  class="col"
+                  :rules="[val => !!val || 'Field is required']"
+                />
               </div>
-              <div class="row items-center">
-                <q-icon name="description" class="col-auto q-mr-md" size="sm" />
-                <q-input dense v-model="description" label="Description" class="col" />
+              <div class="row">
+                <q-icon name="description" class="col-auto q-mr-md q-mt-sm" size="sm" />
+                <q-input
+                  dense
+                  v-model="description"
+                  label="Description"
+                  class="col"
+                  :rules="[val => true]"
+                />
               </div>
-              <div class="row items-center">
-                <q-icon name="attach_file" class="col-auto q-mr-md" size="sm" />
-                <q-file dense v-model="logo" label="Logo" class="col" />
+              <div class="row">
+                <q-icon name="attach_file" class="col-auto q-mr-md q-mt-sm" size="sm" />
+                <q-file dense v-model="logo" label="Logo" class="col" :rules="[val => true]" />
               </div>
               <div class="row">
                 <q-icon name="link" class="col-auto q-mr-md q-mt-sm" size="sm" />
                 <div class="col">
-                  <div v-for="(field, idx) in sites" :key="field.id" class="row items-center q-gutter-sm q-mb-md">
-                    <q-input dense v-model="field.value" class="col" label="Site" />
-                    <div class="row items-center justify-between q-gutter-x-sm buttons-group">
+                  <div v-for="(field, idx) in sites" :key="field.id" class="row items-center q-gutter-sm">
+                    <q-input
+                      dense
+                      v-model="field.value"
+                      class="col"
+                      label="Site"
+                      :rules="[val => true]"
+                    />
+                    <div class="row q-mb-lg justify-between q-gutter-x-sm buttons-group">
                       <q-btn
                         dense
                         v-if="sites.length > 1"
@@ -50,16 +68,24 @@
               <div class="row q-mt-none">
                 <q-icon name="email" class="col-auto q-mr-md q-mt-sm" size="sm" />
                 <div class="col">
-                  <div v-for="(field, idx) in emails" :key="field.id" class="row items-center q-gutter-sm q-mb-md">
+                  <div v-for="(field, idx) in emails" :key="field.id" class="row items-center q-gutter-sm">
                     <q-input
                       dense
                       v-model="field.value"
                       type="email"
                       class="col"
                       label="Email"
+                      :rules="[val => !!val || 'Field is required']"
                     />
-                    <q-input dense v-model="field.comment" type="email" class="col" label="Comment" />
-                    <div class="row items-center justify-between q-gutter-x-sm buttons-group">
+                    <q-input
+                      dense
+                      v-model="field.comment"
+                      type="email"
+                      class="col"
+                      label="Comment"
+                      :rules="[val => true]"
+                    />
+                    <div class="row q-mb-lg justify-between q-gutter-x-sm buttons-group">
                       <q-btn
                         dense
                         v-if="emails.length > 1"
@@ -85,16 +111,24 @@
               <div class="row q-mt-none">
                 <q-icon name="call" class="col-auto q-mr-md q-mt-sm" size="sm" />
                 <div class="col">
-                  <div v-for="(field, idx) in phones" :key="field.id" class="row items-center q-gutter-sm q-mb-md">
+                  <div v-for="(field, idx) in phones" :key="field.id" class="row items-center q-gutter-sm">
                     <q-input
                       dense
                       v-model="field.value"
                       type="tel"
                       class="col"
                       label="Phone"
+                      :rules="[val => true]"
                     />
-                    <q-input dense v-model="field.comment" type="tel" class="col" label="Comment" />
-                    <div class="row items-center justify-between q-gutter-x-sm buttons-group">
+                    <q-input
+                      dense
+                      v-model="field.comment"
+                      type="tel"
+                      class="col"
+                      label="Comment"
+                      :rules="[val => true]"
+                    />
+                    <div class="row q-mb-lg justify-between q-gutter-x-sm buttons-group">
                       <q-btn
                         dense
                         v-if="phones.length > 1"
@@ -117,9 +151,15 @@
                   </div>
                 </div>
               </div>
-              <div class="row q-mt-none items-center">
-                <q-icon name="account_balance_wallet" class="col-auto q-mr-md" size="sm" />
-                <q-input dense v-model="wallet" label="Wallet" class="col" />
+              <div class="row q-mt-none">
+                <q-icon name="account_balance_wallet" class="col-auto q-mr-md q-mt-sm" size="sm" />
+                <q-input
+                  dense
+                  v-model="wallet"
+                  label="Wallet"
+                  class="col"
+                  :rules="[val => true]"
+                />
               </div>
               <div class="row justify-end">
                 <q-btn label="Cancel" type="reset" color="primary" flat />
@@ -129,6 +169,17 @@
           </q-card-section>
         </q-card>
       </div>
+      <q-dialog v-model="error">
+        <q-card class="error-alert">
+          <q-card-section>
+            <div class="text-h6">Error</div>
+          </q-card-section>
+          <q-card-section class="q-pt-none">{{ errorMessage }}</q-card-section>
+          <q-card-actions align="right">
+            <q-btn flat label="OK" color="primary" v-close-popup />
+          </q-card-actions>
+        </q-card>
+      </q-dialog>
     </div>
   </div>
 </template>
@@ -154,6 +205,8 @@ export default {
         { type_id: 2, value: '', comment: '' },
       ],
       wallet: '',
+      error: false,
+      errorMessage: '',
     };
   },
   methods: {
@@ -169,13 +222,17 @@ export default {
       });
     },
     async updateSettings() {
-      await this.$svc.seller.updateSettings({
+      const response = await this.$svc.seller.updateSettings({
         name: this.name,
         description: this.description,
         logo_url: window.location + (this.logo?.name || ''),
         wallet_for_payments_erc20: this.wallet,
         contacts: [...this.sites, ...this.emails, ...this.phones],
       });
+      if (response.status !== 'OK') {
+        this.errorMessage = response.error.msg;
+        this.error = true;
+      }
     },
   },
   async beforeCreate() {
@@ -195,7 +252,8 @@ export default {
       const emails = contacts.filter((contact) => contact.type_id === 3);
       if (emails.length) this.emails = emails;
     } else {
-      console.error(response.error.msg);
+      this.errorMessage = response.error.msg;
+      this.error = true;
     }
   },
   components: {
@@ -208,5 +266,9 @@ export default {
 <style scoped>
   .buttons-group {
     width: 85px
+  }
+  .error-alert {
+    width: 700px;
+    max-width: 80vw;
   }
 </style>
