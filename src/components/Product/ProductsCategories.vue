190cda6ad2035dac76aefd9e824f26aa30eb537d
Andrei Popov <andrepopoff@gmail.com>
2021-10-10 18:59:21 +0300

    [EF-13] Show subcategories

--- src/components/Product/ProductsCategories.vue
+++ src/components/Product/ProductsCategories.vue
@@ -1,16 +1,23 @@
 <template>
-  <q-list bordered padding class="rounded-borders text-primary q-mt-md q-ml-md">
+  <q-list bordered class="rounded-borders text-primary q-mt-md q-ml-md">
     <q-item-label header>All categories</q-item-label>
     <div v-for="category in mainCategories" :key="category.id">
-      <q-item
-        clickable
-        v-ripple
-        :active="activeCategory === category.name"
-        @click="activeCategory = category.name"
-        active-class="active-category"
+      <q-expansion-item
+        expand-separator
+        :label="category.name"
       >
-        <q-item-section>{{ category.name }}</q-item-section>
-      </q-item>
+        <div v-for="subcategory in getChildCategories(category.id)" :key="subcategory.id">
+          <q-item
+            clickable
+            v-ripple
+            :active="activeCategory === subcategory.name"
+            @click="activeCategory = subcategory.name"
+            active-class="active-category"
+          >
+            <q-item-section class="q-pl-md">{{ subcategory.name }}</q-item-section>
+          </q-item>
+        </div>
+      </q-expansion-item>
     </div>
   </q-list>
 </template>
@@ -26,6 +33,14 @@ export default {
   computed: {
     mainCategories() {
       return this.$store.getters['common/getMainCategories'];
+    },
+    allCategories() {
+      return this.$store.getters['common/allCategories'];
+    },
+  },
+  methods: {
+    getChildCategories(categoryID) {
+      return this.allCategories.filter((category) => category.parent_id === categoryID);
     },
   },
 };
