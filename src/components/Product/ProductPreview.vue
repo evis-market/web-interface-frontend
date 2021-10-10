34dd9a71ccbb0d9de604a48f2fdbea3973e93f28
Andrei Popov <andrepopoff@gmail.com>
2021-10-10 16:20:57 +0300

    [EF-13] Layout product preview

--- src/components/Product/ProductPreview.vue
+++ src/components/Product/ProductPreview.vue
@@ -1,5 +1,43 @@
 <template>
-    <div>hello</div>
+  <q-card class="q-mb-lg" flat bordered>
+    <q-card-section horizontal>
+      <q-card-section class="col-4 column">
+        <q-icon name="polymer" size="xl" color="primary" class="q-mb-auto" />
+        <p class="q-mb-none q-mt-md">Data types:</p>
+      </q-card-section>
+
+      <q-card-section class="col-4">
+        <div class="q-mb-auto">
+          <h5 class="q-ma-none">Neighborhood Data</h5>
+          <p>by TovoData</p>
+          <article>
+            discover: Recent comp sales
+            Comp owner name Comp sale
+            price Comp bed / bath Owner
+            occupied Comp taxes Market
+          </article>
+        </div>
+        <p class="q-mb-none q-mt-md">XLS, XML</p>
+      </q-card-section>
+
+      <q-separator vertical />
+
+      <q-card-section class="column full-width items-center">
+        <p>Pricing available upon request</p>
+        <q-btn-group class="column" outline>
+          <q-btn color="red" label="Request sample" class="q-mb-md" />
+          <q-btn color="purple" label="View product" class="q-mb-auto" />
+        </q-btn-group>
+        <q-rating
+          size="2em"
+          color="orange"
+          readonly
+          :model-value="0"
+          class="q-mt-md"
+        />
+      </q-card-section>
+    </q-card-section>
+  </q-card>
 </template>
 
 <script>
