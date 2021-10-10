425b5893131e4257d08ce86743476422640b9047
Andrei Popov <andrepopoff@gmail.com>
2021-10-10 18:29:48 +0300

    [EF-13] Show real categories in the left sidebar

--- src/components/Product/ProductsCategories.vue
+++ src/components/Product/ProductsCategories.vue
@@ -1,55 +1,17 @@
 <template>
   <q-list bordered padding class="rounded-borders text-primary q-mt-md q-ml-md">
     <q-item-label header>All categories</q-item-label>
-    <q-item
-      clickable
-      v-ripple
-      :active="link === 'inbox'"
-      @click="link = 'inbox'"
-      active-class="active-menu-link"
-    >
-      <q-item-section>Inbox</q-item-section>
-    </q-item>
-
-    <q-item
-      clickable
-      v-ripple
-      :active="link === 'outbox'"
-      @click="link = 'outbox'"
-      active-class="active-menu-link"
-    >
-      <q-item-section>Outbox</q-item-section>
-    </q-item>
-
-    <q-item
-      clickable
-      v-ripple
-      :active="link === 'trash'"
-      @click="link = 'trash'"
-      active-class="active-menu-link"
-    >
-      <q-item-section>Trash</q-item-section>
-    </q-item>
-
-    <q-item
-      clickable
-      v-ripple
-      :active="link === 'settings'"
-      @click="link = 'settings'"
-      active-class="active-menu-link"
-    >
-      <q-item-section>Settings</q-item-section>
-    </q-item>
-
-    <q-item
-      clickable
-      v-ripple
-      :active="link === 'help'"
-      @click="link = 'help'"
-      active-class="active-menu-link"
-    >
-      <q-item-section>Help</q-item-section>
-    </q-item>
+    <div v-for="category in mainCategories" :key="category.id">
+      <q-item
+        clickable
+        v-ripple
+        :active="link === category.name"
+        @click="link = category.name"
+        active-class="active-menu-link"
+      >
+        <q-item-section>{{ category.name }}</q-item-section>
+      </q-item>
+    </div>
   </q-list>
 </template>
 
@@ -58,8 +20,13 @@ export default {
   name: 'ProductsCategories',
   data() {
     return {
-      link: 'inbox',
+      link: 'Financial Market Data',
     };
+  },
+  computed: {
+    mainCategories() {
+      return this.$store.getters['common/getMainCategories'];
+    },
   },
 };
 </script>
