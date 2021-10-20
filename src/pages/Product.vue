c1555e14f2a4ccda34db32ebc2094900223af75b
Andrei Popov <andrepopoff@gmail.com>
2021-10-20 23:17:36 +0300

    [EF-19] Add related products

--- src/pages/Product.vue
+++ src/pages/Product.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>Hello world</div>
+  <q-card>
+    <q-card-section>
+      <div class="text-h5">Related Products</div>
+      <ProductPreview :product="{ name: 'Some product', id: 100 }"/>
+      <ProductPreview :product="{ name: 'Related product', id: 101 }" />
+    </q-card-section>
+  </q-card>
 </template>
 
 <script>
+import ProductPreview from 'components/Product/ProductPreview';
+
 export default {
   name: 'Product',
+  components: { ProductPreview },
 };
 </script>
 
