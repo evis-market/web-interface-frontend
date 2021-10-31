8efba1363d7ba419e12b6f65ef72a200d96a8daa
Andrei Popov <andrepopoff@gmail.com>
2021-10-31 22:56:42 +0300

    [EF-57] Add pages links to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,22 @@
   <footer class="app-footer">
     <div class="app-footer__decor"></div>
     <div class="app-footer__content">
-      <div class="app-footer__pages"></div>
+      <div class="app-footer__pages text-subhead-2-medium">
+        <ul class="app-footer__pages-list q-pr-xl">
+          <li><a target="_blank" href="https://evis.market/pdf/white_paper_ru.pdf">White Paper</a></li>
+          <li><a target="_blank" href="https://evisdm.medium.com/">Blog</a></li>
+          <li><a target="_blank" href="https://t.me/igor_yur">Contact</a></li>
+          <li><a target="_blank" href="#">FAQ</a></li>
+          <li><a target="_blank" href="https://evis.market/ru/team/">Team</a></li>
+        </ul>
+        <ul class="app-footer__pages-list">
+          <li><a target="_blank" href="https://evis.market/pdf/privacy_policy_en.pdf">Privacy</a></li>
+          <li><a target="_blank" href="https://evis.market/pdf/terms_of_service_en.pdf">Terms</a></li>
+          <li><a target="_blank" href="https://evis.market/pdf/cookie_policy_en.pdf">Cookie Policy</a></li>
+          <li><a target="_blank" href="https://evis.market/pdf/ccpa_en.pdf">CCPA</a></li>
+          <li><a target="_blank" href="https://evis.market/pdf/risk_disclaimer_en.pdf">Risk disclaimer</a></li>
+        </ul>
+      </div>
       <div class="app-footer__contacts"></div>
     </div>
   </footer>
@@ -17,20 +32,32 @@ export default {
 <style lang="scss">
   .app-footer {
     flex-shrink: 0;
-  &__decor {
-     -webkit-transform: skew(45deg);
-     -ms-transform: skew(45deg);
-     transform: skew(45deg);
-     background-color: $primary;
-     height: 80px;
-     width: 55%;
-     margin-left: -80px;
-   }
-  &__content {
-     margin-right: -20px;
-     margin-left: -20px;
-     min-height: 200px;
-     background-color: $primary;
-   }
+    &__decor {
+      -webkit-transform: skew(45deg);
+      -ms-transform: skew(45deg);
+      transform: skew(45deg);
+      background-color: $primary;
+      height: 80px;
+      width: 55%;
+      margin-left: -80px;
+    }
+    &__content {
+      margin-right: -20px;
+      margin-left: -20px;
+      min-height: 200px;
+      background-color: $primary;
+      padding-top: 8px;
+      padding-left: 400px;
+    }
+    &__pages {
+      display: flex;
+      & ul {
+        list-style: none;
+      }
+      & a {
+        text-decoration: none;
+        color: white;
+      }
+    }
   }
 </style>
